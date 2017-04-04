f79612e927fb0e7deef88cb6f9534f0109dfdb4d
Suzanne <suzanne.aitchison@gmail.com>
2017-04-04 18:49:25 +0100

    can delete all dinos of a type

--- park.js
+++ park.js
@@ -6,6 +6,21 @@ var Park = function(){
 Park.prototype = {
   addToEnclosure: function(dinosaur){
     this.enclosure.push(dinosaur)
+  },
+
+  removeAllTypeFromEnclosure(type){
+    var dinosToDelete = []
+    for (var i = 0; i < this.enclosure.length; i++){
+      var dinosaur = this.enclosure[i]
+      if (dinosaur.type === type){
+        dinosToDelete.push(dinosaur)
+      }
+    }
+
+    for (dino of dinosToDelete){
+      var index = this.enclosure.indexOf(dino)
+      this.enclosure.splice(index, 1)
+    }
   }
 }
 
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -5,11 +5,15 @@ var Dinosaur = require('../dinosaur.js')
 describe('Park', function(){
 
   var park
-  var dinosaur
+  var plesiosaur
+  var pterodactyl
+  var stegosaurus
 
   beforeEach(function(){
     park = new Park
-    dinosaur = new Dinosaur("Plesiosaur", 4)
+    plesiosaur = new Dinosaur("Plesiosaur", 4)
+    pterodactyl = new Dinosaur("Pterodactyl", 1)
+    stegosaurus = new Dinosaur("Stegosaurus", 3)
   })
 
   it('starts with empty enclosure', function(){
@@ -17,7 +21,18 @@ describe('Park', function(){
   })
 
   it('can add a dinosaur to enclosure', function(){
-    park.addToEnclosure(dinosaur)
+    park.addToEnclosure(plesiosaur)
     assert.strictEqual(1, park.enclosure.length)
+  })
+
+  it('can remove all dinosaurs of a type from enclosure', function(){
+    park.addToEnclosure(plesiosaur)
+    park.addToEnclosure(plesiosaur)
+    park.addToEnclosure(stegosaurus)
+    park.addToEnclosure(stegosaurus)
+    park.addToEnclosure(stegosaurus)
+    park.addToEnclosure(pterodactyl)
+    park.removeAllTypeFromEnclosure('Plesiosaur')
+    assert.strictEqual(4, park.enclosure.length)
   })
 })
