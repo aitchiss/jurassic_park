a27a47ce952cae3664edfa656af6bfd6166e50b3
Suzanne <suzanne.aitchison@gmail.com>
2017-04-04 18:28:25 +0100

    dinosaur has number of offspring per year

--- dinosaur.js
+++ dinosaur.js
@@ -1,6 +1,9 @@
 
-var Dinosaur = function(type){
+var Dinosaur = function(type, numberOfOffspring){
   this.type = type
+  this.offspringPerYear = numberOfOffspring
 }
+
+
 
 module.exports = Dinosaur
--- specs/dinosaur_spec.js
+++ specs/dinosaur_spec.js
@@ -6,11 +6,15 @@ describe('Dinosaur', function(){
   var dinosaur
 
   beforeEach(function(){
-    dinosaur = new Dinosaur('Brachiosaurus');
+    dinosaur = new Dinosaur('Brachiosaurus', 3);
   })
 
   it('has a type', function(){
     assert.strictEqual('Brachiosaurus', dinosaur.type)
+  })
+
+  it('has a number of offspring per year', function(){
+    assert.strictEqual(3, dinosaur.offspringPerYear)
   })
 
 })
